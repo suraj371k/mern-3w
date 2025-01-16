a11977d03dc9198d2e3afdfb3011b0a876bcfdae
suraj371k <surajkushwaha371@gmail.com>
2025-01-16 11:28:57 +0530

    improving code

--- server/server.js
+++ server/server.js
@@ -2,23 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path'); 
 const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+
+const uploadsPath = path.join(__dirname, 'uploads'); // Dynamically resolve the path
+app.use('/uploads', express.static(uploadsPath));
+
 app.use('/api/users', userRoutes);
 
 mongoose
-  .connect(`${process.env.MONGO_URI}`)
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error('MongoDB connection error:', err));
